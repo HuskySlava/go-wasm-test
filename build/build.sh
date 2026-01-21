f54bff215791e121fc3dcd86bbb5b727ce342aa5
Slava Mihaelov <huskyslava@gmail.com>
2026-01-21 13:29:32 +0200

    Build script

---
+++ build/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Terminal color variables
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+RED="\033[1;31m"
+RESET="\033[0m"
+
+
+## Compile WASM
+printf "${YELLOW}Compiling WASM..${RESET}\n"
+cd go-src # Move to GO folder
+make wasm
+
+## Copy WASM
+printf "${YELLOW}Copying WebAssembly to dist...${RESET}\n"
+cp dist/* ../dist
+cp wasm_exec.js ../dist # WASM Runtime
+rm -rf dist
+
+cd .. # Move back to root
+
+## Compile TypeScript
+printf "${YELLOW}Compiling TypeScript...${RESET}\n"
+tsc
+
+## Copy html
+printf "${YELLOW}Copying HTML...${RESET}\n"
+cd ts-src
+cp index.html ../dist
+printf "\033[1;32mDone\033[0m\n"
+
+
+
+
